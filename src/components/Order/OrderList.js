b832d4360c3bae9ed299018685b19803747d2686
mfikria <mfikria@gmail.com>
2022-01-28 16:08:14 +0700

    feat: implement order list

--- src/components/Order/OrderList.js
+++ src/components/Order/OrderList.js
@@ -1,33 +1,73 @@
-import { Table } from 'antd';
-import Link from 'next/link';
+import { Descriptions, Layout, Table, Typography, Tag, Tabs, Breadcrumb } from 'antd'
 import { useSelector } from 'react-redux';
+import moment from 'moment'
 
 const columns = [{
-  title: 'Ordername',
-  dataIndex: 'ordername',
-  key: 'ordername',
-  render: (text) => (
-    <Link href={`/order/[ordername]`} as={`/order/${text}`}>
-      <a>{text}</a>
-    </Link>
-  )
+  title: 'Date',
+  dataIndex: 'dueDate',
+  key: 'dueDate',
+  render: (date) => moment(date).format('DD MMM YYYY')
 }, {
-  title: 'Email',
-  dataIndex: 'email',
-  key: 'email',
+  title: 'Order Status',
+  dataIndex: 'status',
+  key: 'status',
+}, {
+  title: 'Customer',
+  dataIndex: 'customer',
+  key: 'customer',
+  render: (customer) => customer.name.firstname
+}, {
+  title: 'Total  Price',
+  dataIndex: 'totalAmount',
+  key: 'totalAmount',
+  render: (totalAmount) => `Rp${totalAmount}`
+}, {
+  title: 'Payment Status',
+  dataIndex: 'status',
+  key: 'status',
+  render: (status) => <Tag color="green">{status}</Tag>
+}, {
+  title: 'Payment Method',
+  dataIndex: 'paymentMethod',
+  key: 'paymentMethod',
+  render: (status) => <Tag color="green">{status}</Tag>
 }];
 
 const OrderList = () => {
   const list = useSelector(state => state.order.list);
 
   return (
-    <Table
-      rowKey={record => record.id}
-      style={{ minWidth: '600px' }}
-      dataSource={list}
-      columns={columns}
-      bordered
-    />
+    <Layout>
+      <Layout.Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        <div style={{ padding: 24, minHeight: 380 }}>
+          <div className="mb-8">
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Orders</Breadcrumb.Item>
+              <Breadcrumb.Item>Active Orders</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+
+          <Tabs defaultActiveKey="1">
+            <Tabs.TabPane tab="Active Orders" key="1">
+              <Table
+                rowKey={record => record.id}
+                style={{ minWidth: '600px' }}
+                dataSource={list}
+                columns={columns}
+                bordered
+              />
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Completed" disabled key="2">
+              Tab 2
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Cancelled" disabled key="3">
+              Tab 3
+            </Tabs.TabPane>
+          </Tabs>
+        </div>
+      </Layout.Content>
+      <Layout.Footer style={{ textAlign: 'center' }}>Xendit ©2022</Layout.Footer>
+    </Layout>
   );
 };
 
